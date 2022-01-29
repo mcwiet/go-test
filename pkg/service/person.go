748bc3a842ac7d58214d53ed30b2504010a0319e
Mike Cwietniewicz <mike.cwiet@gmail.com>
2022-01-29 14:49:32 -0500

    Moving some logic around now that basics are working, moving to cleaner architecture. Update for some minimal error handling

--- pkg/service/person.go
+++ pkg/service/person.go
@@ -5,10 +5,10 @@ import (
 	"github.com/mcwiet/go-test/pkg/model"
 )
 
-func GetPerson(id string) model.Person {
+func GetPerson(id string) (model.Person, error) {
 	return data.GetPerson(id)
 }
 
-func GetPeople() *[]model.Person {
+func GetPeople() ([]model.Person, error) {
 	return data.GetPeople()
 }
