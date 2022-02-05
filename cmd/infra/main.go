527d021261e32d45321ab49856c699d6f569333e
mcwiet <52891812+mcwiet@users.noreply.github.com>
2022-02-04 22:03:35 -0500

    CICD Updates (#7)
    * Splitting out the jobs of the pipeline
    
    * Adding env to naming of infrastructure assets
    
    * Initial try for integration tests in pipeline
    
    * Fixing error in workflow
    
    * Making integration tests depend on infra deployment
    
    * Adding AWS CDK for infra deploy. Fixing integration test command.
    
    * Concurrency restriction test 1
    
    * Concurrency restriction test 2
    
    * Attempting to split workflows into separate files
    
    * Removing manual set of GITHUB_TOKEN
    
    * Testing split workflows
    
    * Removing old main workflow
    
    * Changing how environment is passed to reusable workflows
    
    * Known bug with reusable workflows, restorting to duplicated code for now
    
    * Full workflow for pull request to main
    
    * Environment value cannot be pulled from workflow environment?
    
    * Adding workflow for production deploy (on push to main)
    
    * Fixing job dependency error
    
    * Only deploying if unit tests pass

--- cmd/infra/main.go
+++ cmd/infra/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/mcwiet/go-test/pkg/infra"
 )
@@ -8,10 +10,15 @@ import (
 func main() {
 	app := awscdk.NewApp(nil)
 
-	apiStackName := "go-api"
+	env := os.Getenv("ENV")
+	if env == "" {
+		env = "development"
+	}
+
+	apiStackName := "go-api-" + env
 	infra.NewApiStack(app, apiStackName, &awscdk.StackProps{
 		StackName: &apiStackName,
-		Env:       env(),
+		Env:       newCdkEnvironment(),
 	})
 
 	app.Synth(nil)
@@ -19,27 +26,16 @@ func main() {
 
 // env determines the AWS environment (account+region) in which our stack is to
 // be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
+func newCdkEnvironment() *awscdk.Environment {
+	awsAccount := os.Getenv("AWS_ACCOUNT")
+	awsRegion := os.Getenv("AWS_REGION")
 
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	// 	Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	// 	Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
+	if awsAccount != "" && awsRegion != "" {
+		return &awscdk.Environment{
+			Account: &awsAccount,
+			Region:  &awsRegion,
+		}
+	} else {
+		return nil
+	}
 }
